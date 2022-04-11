b1544e88661a7888011c498ee1a2c17649915348
unknown <dineshjmsbw25@gmail.com>
2022-04-11 19:06:12 +0530

    Spring Tests

---
+++ src/main/java/com/playground/TestClass.java
@@ -0,0 +1,13 @@
+package com.playground;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Triangle obj = new Triangle();
+		obj.setType("Eqvilateral");
+		
+		System.out.println(obj);
+	}
+
+}
---
+++ src/main/java/com/playground/Triangle.java
@@ -0,0 +1,16 @@
+package com.playground;
+
+
+
+public class Triangle {
+
+	String type;
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String toString() {
+		return "Triangle Type is : " + type;
+	}
+}
